75f72349d307c33a5e32c3253126339e78a4123e
samadova.minira <minira.samadova@syncthought.com>
2019-05-31 18:36:32 +0900

    still thinking

--- 5-hard-shit-with-promises/5-hard-shit-with-promises.js
+++ 5-hard-shit-with-promises/5-hard-shit-with-promises.js
@@ -7,4 +7,31 @@ function translateToSpanish(word) {
   });
 }
 
-console.log(translateToSpanish("hello"));
+let words = ["alarm","circuit", "bus", "system", "firewall"];
+
+//, "feed", "firewall", "panel", "alarm", "matrix", "matrix", "array", "feed", "microchip", "interface", "interface", "program", "feed", "pixel", "matrix"
+debugger;
+
+var result
+
+function waitForEveryWord(array) {
+  let result = [];
+  for(var i = 0; i < array.length; i++) {
+    translateToSpanish(array[i])
+    .then(translatedWord => result.push(translatedWord))
+    .catch(err => console.log(err));
+  }
+  return result;
+}
+
+console.log(waitForEveryWord(words));
+
+function waitForEveryWord(array) {
+
+}
+
+new Promise(resolve => {
+  translateToSpanish(word)
+  .then(translation => result.push(translation))
+  .catch(err => console.log(err));
+});
