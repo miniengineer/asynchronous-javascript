9d01ec630d4c690146044bfde64cc213478018a3
samadova.minira <minira.samadova@syncthought.com>
2019-05-31 19:00:06 +0900

    think I am getting closer

--- 5-hard-shit-with-promises/5-hard-shit-with-promises.js
+++ 5-hard-shit-with-promises/5-hard-shit-with-promises.js
@@ -10,28 +10,19 @@ function translateToSpanish(word) {
 let words = ["alarm","circuit", "bus", "system", "firewall"];
 
 //, "feed", "firewall", "panel", "alarm", "matrix", "matrix", "array", "feed", "microchip", "interface", "interface", "program", "feed", "pixel", "matrix"
-debugger;
 
-var result
+function goToNext(word) {
+  return new Promise(resolve => {
+    resolve(translateToSpanish(word).then(translation => result.push(translation)).catch(err => console.log(err)));
+  });
+}
+var result = [];
 
 function waitForEveryWord(array) {
-  let result = [];
-  for(var i = 0; i < array.length; i++) {
-    translateToSpanish(array[i])
-    .then(translatedWord => result.push(translatedWord))
-    .catch(err => console.log(err));
-  }
+  words.forEach(word => {
+    goToNext(word);
+  });
   return result;
 }
 
 console.log(waitForEveryWord(words));
-
-function waitForEveryWord(array) {
-
-}
-
-new Promise(resolve => {
-  translateToSpanish(word)
-  .then(translation => result.push(translation))
-  .catch(err => console.log(err));
-});
