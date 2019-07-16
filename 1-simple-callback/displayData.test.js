62239c8d194220359fe364d2bf8b007208a7c6b2
Minira Samadova <samadova.minira@gmail.com>
2019-07-17 07:49:36 +0900

    updated lodash and npm

--- 1-simple-callback/displayData.test.js
+++ 1-simple-callback/displayData.test.js
@@ -7,7 +7,7 @@ var accounts = [
 
 accounts = JSON.stringify(accounts);
 
-test('should take parsed array and render name, id and phone number', () => {
+test('should parse a JSON file and render name, id and phone number', () => {
   const text = displayData(accounts);
   expect(text).toStrictEqual([`Brent Romaguera has an id of 1 and a phone number of 372-746-9818`,
      `Aryanna Adams has an id of 2 and a phone number of (956) 491-7705`]);
