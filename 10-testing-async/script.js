a226f9b510c81e56d923c222f40a02a45dc4a59e
Minira Samadova <samadova.minira@gmail.com>
2020-01-31 17:37:38 +0900

    deleted-unnecessary-function-invocation

--- 10-testing-async/script.js
+++ 10-testing-async/script.js
@@ -8,7 +8,6 @@ const fetchData = (url) => {
   return axios.get(url).then(response => response.data);
 }
 
-// fetchData('https://5e33e13be0161c00140ac5b8.mockapi.io/users');
 
 module.exports = {
   sum,
